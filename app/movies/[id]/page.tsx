3dabbc46bdbd9f38ccf32fee580a128d6cbd6eb7
smd995 <112795934+smd995@users.noreply.github.com>
2025-01-03 12:04:54 +0900

    first commit

---
+++ app/movies/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../components/movie-info";
+import MovieVideos from "../../../components/movie-videos";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = await params;
+
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
+}
