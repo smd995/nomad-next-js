c8c9a9e10fda8aab1a54748ba0aaf858db83152b
smd995 <112795934+smd995@users.noreply.github.com>
2025-01-03 12:20:48 +0900

    any

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -2,15 +2,13 @@ import { Suspense } from "react";
 import MovieInfo from "../../../components/movie-info";
 import MovieVideos from "../../../components/movie-videos";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-
+export default function Page({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
-      <Suspense fallback={<h1>Loading movie info</h1>}>
+      <Suspense fallback={<h1>Loading movie info...</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <Suspense fallback={<h1>Loading movie videos</h1>}>
+      <Suspense fallback={<h1>Loading movie videos...</h1>}>
         <MovieVideos id={id} />
       </Suspense>
     </div>
